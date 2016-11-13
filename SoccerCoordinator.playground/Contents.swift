ea7a66e7b1fe45c5229ec93d328e70a23ff6d0b9
Cori Mims <cdmims21@gmail.com>
2016-11-13 15:17:27 -0500

    Added arrays for the three teams and the logic to loop through each player and add them to the teams

--- SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator.playground/Contents.swift
@@ -22,6 +22,32 @@ let player18:[String: String] = ["Name": "Herschel Krustofski", "Height": "45", 
 // Array Of Dictionaries For Entire League
 let entireLeague: [[String: String]] = [player1, player2, player3, player4, player5, player6, player7, player8, player9, player10, player11, player12, player13, player14, player15, player16, player17, player18]
 
+// Array Of Teams
+var teamDragons: [[String: String]] = []
+var teamSharks: [[String: String]] = []
+var teamRaptors: [[String: String]] = []
+
+// Loop Through each player to add players with experience first.
+for index in 0..<entireLeague.count {
+    if teamDragons.count<3 && entireLeague[index]["Experience"] == "YES" {
+        teamDragons.append(entireLeague[index])
+    }else if teamSharks.count<3 && entireLeague[index]["Experience"] == "YES" {
+        teamSharks.append(entireLeague[index])
+    }else if teamRaptors.count<3 && entireLeague[index]["Experience"] == "YES"{
+        teamRaptors.append(entireLeague[index])
+    }
+}
+
+// Loop through each player to add players without experience
+for index in 0..<entireLeague.count {
+    if teamDragons.count<6 && entireLeague[index]["Experience"] == "NO" {
+        teamDragons.append(entireLeague[index])
+    }else if teamSharks.count<6 && entireLeague[index]["Experience"] == "NO" {
+        teamSharks.append(entireLeague[index])
+    }else if teamRaptors.count<6 && entireLeague[index]["Experience"] == "NO"{
+        teamRaptors.append(entireLeague[index])
+    }
+}
 
 
 
